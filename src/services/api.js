277a5fd8948bb231a07e35f0d8f0ab2d01e9cd09
PaNkaj44 <bamniya853@gmail.com>
2026-06-28 16:31:40 +0530

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://booking-backend-9ld0.onrender.com/api';
 import axios from 'axios';
 
 const apiClient = axios.create({
